9499ce7d0af0fe3956a1bfc0da7c48fb6f8e2f02
Crowdbotics <team@crowdbotics.com>
2021-01-11 03:59:34 +0000

    Update components

--- src/features/BlankScreen0191567/screens/index.js
+++ src/features/BlankScreen0191567/screens/index.js
@@ -21,20 +21,26 @@ export class Blank extends React.Component {
   static navigationOptions = ({ navigation }) => {
     return { headerLeft: <SlideMenuIcon navigationProps={navigation} /> }
   }
-  state = { TextInput_3: "", TextInput_5: "" }
+  state = { TextInput_5: "", TextInput_3: "" }
   render = () => (
     <View style={styles.View_1}>
+      <TextInput
+        placeholder="Password"
+        style={styles.TextInput_5}
+        value={this.state.TextInput_5}
+        onChangeText={nextValue => this.setState({ TextInput_5: nextValue })}
+      />
       <TextInput
         placeholder="Email"
         style={styles.TextInput_3}
         value={this.state.TextInput_3}
         onChangeText={nextValue => this.setState({ TextInput_3: nextValue })}
       />
-      <TextInput
-        placeholder="Password"
-        style={styles.TextInput_5}
-        value={this.state.TextInput_5}
-        onChangeText={nextValue => this.setState({ TextInput_5: nextValue })}
+      <Button
+        title="Submit"
+        color="#aaacb1"
+        style={styles.Button_9}
+        onPress={() => alert("Pressed!")}
       />
     </View>
   )
@@ -44,29 +50,23 @@ const styles = StyleSheet.create({
   View_1: {
     overflow: "scroll",
     alignSelf: "center",
-    flexWrap: "nowrap",
-    justifyContent: "center",
+    flexWrap: "wrap",
+    justifyContent: "flex-start",
     alignItems: "center",
-    alignContent: "center"
-  },
-  TextInput_3: {
-    width: 200,
-    height: 0,
-    marginTop: "0%",
-    marginBottom: "30%",
-    alignSelf: "center",
-    fontFamily: "Merriweather-Bold",
-    borderWidth: 2,
-    borderLeftWidth: 2,
-    borderRightWidth: 2,
-    borderTopWidth: 2,
-    borderBottomWidth: 2
+    alignContent: "center",
+    borderColor: "#5c5c5c",
+    borderStyle: "dashed",
+    borderWidth: 1,
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderTopWidth: 1,
+    borderBottomWidth: 1
   },
   TextInput_5: {
     width: 200,
     height: 0,
-    marginTop: 40,
-    marginBottom: 0,
+    marginTop: "50%",
+    marginBottom: "0%",
     alignSelf: "center",
     fontFamily: "Merriweather-Bold",
     borderWidth: 2,
@@ -74,6 +74,32 @@ const styles = StyleSheet.create({
     borderRightWidth: 2,
     borderTopWidth: 2,
     borderBottomWidth: 2
+  },
+  TextInput_3: {
+    width: 200,
+    height: 0,
+    marginTop: "30%",
+    marginBottom: "0%",
+    alignSelf: "center",
+    fontFamily: "Merriweather-Bold",
+    borderWidth: 2,
+    borderLeftWidth: 2,
+    borderRightWidth: 2,
+    borderTopWidth: 2,
+    borderBottomWidth: 2
+  },
+  Button_9: {
+    width: 50,
+    marginTop: "70%",
+    marginBottom: "0%",
+    alignSelf: "center",
+    fontFamily: "Roboto-Bold",
+    borderColor: "#5f5d5d",
+    borderWidth: 1,
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderTopWidth: 1,
+    borderBottomWidth: 1
   }
 })
 
